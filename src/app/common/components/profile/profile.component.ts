c8b115311dfe4e15c0074ef8c85b2c3237f60dfd
Indu04 <indu.104@gmail.com>
2018-03-09 15:57:00 +0530

    Profile object made as input, few minor changes as per review discussion

--- src/app/common/components/profile/profile.component.ts
+++ src/app/common/components/profile/profile.component.ts
@@ -1,23 +1,15 @@
 import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
-  // tslint:disable-next-line:component-selector
   selector: 'db-app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css']
 })
-export class ProfileComponent implements OnInit {
+export class ProfileComponent{
 
   @Input('borderColor') public borderColor = 'grey';
   @Input('size') public size = 'medium';
-
-  // below values come from database
-  profObj = [{userID: '1', name: 'INDU', path: 'assets/ProfileImages/1.png', avatarShape: 'hexagon'},
-             {userID: '2', name: 'DIVS', path: 'assets/ProfileImages/2.png', avatarShape: 'parallelogram'},
-             {userID: '3', name: 'RAKI', path: 'assets/ProfileImages/3.png', avatarShape: 'pentagon'},
-             {userID: '4', name: 'VIJI', path: 'assets/ProfileImages/4.jpg', avatarShape: 'circle-360'},
-             {userID: '5', name: 'RAKS', path: 'assets/ProfileImages/5.jpg', avatarShape: 'square'}
- ];
+  @Input('profileObj') public profileObj;
 
  // returns size in pixel based on large/medium
   public getSize(size: string): string {
@@ -27,11 +19,6 @@ export class ProfileComponent implements OnInit {
     if (size === 'medium') {
         return '100px';
     }
-  }
-
-  constructor() { }
-
-  ngOnInit() {
   }
 
 }
