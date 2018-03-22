4d206d683cfe3acd79cb7e35db22c182b25fef3b
Gautam, Pawan <Pawan.Gautam@emc.com>
2018-03-22 20:00:43 +0530

    Modified table component to take care of table color and border dynamically.

--- src/app/common/components/table/table.component.ts
+++ src/app/common/components/table/table.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
-  selector: 'app-table',
+  selector: 'db-app-table',
   templateUrl: './table.component.html',
   styleUrls: ['./table.component.css']
 })
 export class TableComponent implements OnInit {
+  @Input('tableColor') public tableColor:string = 'blue';
+  @Input('tableBorder') public tableBorder:string = '10px solid yellow';
 
   constructor() { }
 
