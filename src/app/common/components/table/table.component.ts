f10375e042abfa775847bd032b6dca440f97f179
Gautam, Pawan <Pawan.Gautam@emc.com>
2018-03-23 20:35:57 +0530

    taken care of review comments.

--- src/app/common/components/table/table.component.ts
+++ src/app/common/components/table/table.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'db-app-table',
   templateUrl: './table.component.html',
   styleUrls: ['./table.component.css']
 })
-export class TableComponent implements OnInit {
+export class TableComponent implements {
   @Input('tableColor') public tableColor:string = 'blue';
-  @Input('tableBorder') public tableBorder:string = '10px solid yellow';
-
-  constructor() { }
 
   ngOnInit() {
   }
