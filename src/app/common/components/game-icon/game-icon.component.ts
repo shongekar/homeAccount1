fdcbf52f0f761d95b6fc917b2b2d821460291ddf
Sachin Shinde <sachkanch2010@gmail.com>
2018-03-24 12:09:08 +0530

    minor changes

--- src/app/common/components/game-icon/game-icon.component.ts
+++ src/app/common/components/game-icon/game-icon.component.ts
@@ -6,7 +6,6 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./game-icon.component.css']
 })
 export class GameIconComponent {
-  @Input() gameName: string;
   @Input() link: string;
   @Input() gameImage: string;
 }
