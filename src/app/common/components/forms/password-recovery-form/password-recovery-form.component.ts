fd98b97470d0e45bc6f6355110f0949e25b10b7a
Indu04 <indu.104@gmail.com>
2018-03-27 19:28:12 +0530

    Validation added

--- src/app/common/components/forms/password-recovery-form/password-recovery-form.component.ts
+++ src/app/common/components/forms/password-recovery-form/password-recovery-form.component.ts
@@ -5,52 +5,43 @@ import { FormControl, FormGroup, Validators, FormBuilder, ValidatorFn } from '@a
 import { FormDataValidator } from '../../../validators/form-data.validator';
 
 @Component({
-  selector: 'app-password-recovery-form',
+  selector: 'db-password-recovery-form',
   templateUrl: './password-recovery-form.component.html',
   styleUrls: ['./password-recovery-form.component.css']
 })
 export class PasswordRecoveryFormComponent{
 
-  maxLength:number = 15;
-  minLength:number = 8;
-  
   @Input('email') public email:string = 'test@gmail.com';
-  form = new FormGroup({
-    password: new FormControl('', [Validators.required, // Default validation
-                                  ]),
 
-    confirmPassword: new FormControl('', [Validators.required, // Default validation
-                                          FormDataValidator.MatchPassword
-                                         ]), 
-  });
+  form = new FormGroup({password: new FormControl('', [Validators.required,
+                                                         Validators.minLength(6),
+                                                         Validators.maxLength(16),]),
+                        confirmPassword: new FormControl('', [Validators.required,
+                                                              Validators.minLength(6),
+                                                              Validators.maxLength(16),]),
+                        }, this.pwdMatchValidator);
 
-  // form: FormGroup;
-  // password = new FormControl("", [Validators.required]);
-  // confirmPassword = new FormControl("", [Validators.required, this.validatePasswordConfirmation]);
+pwdMatchValidator(frm: FormGroup) {
+    var password = frm.get('password').value;
+    var confirmPassword = frm.get('confirmPassword').value;
+    if(password != confirmPassword) {
+      console.log('false');
+      frm.get('confirmPassword').setErrors( {MatchPassword: true} )}
+      else {
+        console.log('true');
+        return null}}
 
-
-  // constructor(private fb: FormBuilder) {
-  // }
-  // ngOnInit() {
-  //   this.form = this.fb.group({
-  //     "password": this.password,
-  //     "confirmPassword": this.confirmPassword
-  //   },
-  //   {
-  //     validator: this.validatePasswordConfirmation
-  //   });
-  // }
-
-  // validatePasswordConfirmation(group: FormGroup) {
-  //   var pw = group.controls['password'];
-  //   var pw2 = group.controls['passwordConfirm'];
-
-  //   if (pw.value !== pw2.value) { // this is the trick
-  //     pw2.setErrors({validatePasswordConfirmation: true});
-  //   }
-
-  //   // even though there was an error, we still return null
-  //   // since the new error state was set on the individual field
-  //   return null; 
-  // }
+get password() {
+  return this.form.get('password');
 }
+
+get confirmPassword() {
+  return this.form.get('confirmPassword')
+}
+
+submitValue(){
+  console.log(this.form.get('password'));
+}
+
+}
+
