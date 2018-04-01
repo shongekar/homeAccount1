a80d5099f620aebc205f5748f86318ba86eb874d
Venkatesh, Rakshitha <rakshitha.41@gmail.com>
2018-04-01 20:51:02 +0530

    addressed review comments

--- src/app/common/components/navbar/navbar.component.ts
+++ src/app/common/components/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
@@ -6,6 +6,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
+
+  @Input() public playerName: string = "Rakshitha";
+  @Input() public notifications = [ "Notification 1" ]; //For testing notifcation 1 is set
+  
+  color: string = "primary";
+
+  openProfile(){
+    alert("TODO")
+  }
+
+  logout(){
+    
+    let response = confirm("Do you really want to logout?");
+
+    if (response) {
+        alert("Logged out!")
+    } 
+    else {
+      alert("Good! Game On!")
+    }
+
+  }
 
   constructor() { }
 
